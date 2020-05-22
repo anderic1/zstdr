f71b6ee139635422409a0ae71719b61603ad35e8
anderic1 <anderic1@gmx.com>
2020-05-22 10:19:21 +0200

    testthat::is_true() deprecated

--- tests/testthat/test-compress.R
+++ tests/testthat/test-compress.R
@@ -4,10 +4,10 @@ test_that("compression/decompression works", {
 
     # test default compression level, which is 3
     compressed <- zstdCompress(data)
-    expect_that(identical(data, zstdDecompress(compressed)), is_true())
+    expect_true(identical(data, zstdDecompress(compressed)))
 
     for (level in 1:zstdMaxCLevel()) {
         compressed <- zstdCompress(data, level)
-        expect_that(identical(data, zstdDecompress(compressed)), is_true())
+        expect_true(identical(data, zstdDecompress(compressed)))
     }
 })
